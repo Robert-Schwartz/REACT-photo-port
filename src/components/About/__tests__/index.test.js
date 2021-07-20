bac72f71c61a7f557ff7b6b362cb14c965487349
Robert Schwartz <bschwartz23@gmail.com>
2021-07-20 14:19:09 -0700

    complete About comp tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -10,13 +10,13 @@ import '@testing-library/jest-dom/extend-expect';
 afterEach(cleanup);
 //This will ensure that after each test, we won't have any leftover memory data that could give us false results.
 
-// declare the component we're testing
+// declare the component we're testing ('About component')
 describe('About component', () => {
-    //first test
+    //first test, does it render?
     test('renders', () => {
         render(<About />);
     });
-    // second test
+    // second test, create a matching snapshot file 
     test('matches snapshot DOM node structure', () => {
         const { asFragment } = render(<About />);
 
