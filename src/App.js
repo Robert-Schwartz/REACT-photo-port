32862c61728cba173109d6cf23a13cbb5b1495fe
Robert Schwartz <bschwartz23@gmail.com>
2021-07-19 18:23:03 -0700

    Create the About Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// Import React in every component file
+import React from 'react';
 
+// Import Components
+import About from './components/About';
+
+// App function
+// ====================================
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+// Import React
+import React from 'react';
+// Import image
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+function About() {
+    return (
+        <section className="my-5">
+            <h1 id="about">Who am I?</h1>
+        </section>
+    );
+}
+
+// Export default
+export default About;
