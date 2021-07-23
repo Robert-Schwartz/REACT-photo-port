037f495ea1c5ea8f3f7f31b55f4cb809bc7694a4
Robert Schwartz <bschwartz23@gmail.com>
2021-07-22 18:32:44 -0700

    Create the Gallery Component with hardcoded data for testing.

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
+// import helper function for Capitalize First Letter
+import { capitalizeFirstLetter } from '../../utils/helpers';
+// import photo
+import photo from "../../assets/small/commercial/0.jpg";
 
-function Gallery() {
+
+// Gallery Component
+function Gallery(props) {
+    const currentCategory = {
+        name: "commercial",
+        description:
+            "Photos of grocery stores, food trucks, and other commercial projects",
+    };
     return (
         <section>
             {/* we want the name and description of the selected category to show up at the top */}
-            <h1>NAME</h1>
-            <p>DESCRIPTION</p>
+            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+            <p>{currentCategory.name}</p>
+            <div className="flex-row">
+                <img
+                    src={photo}
+                    alt="Commercial Example"
+                    className="img-thumbnail mx-1"
+                />
+            </div>
         </section>
     );
 }
-
 
 // export Gallery
 export default Gallery;
