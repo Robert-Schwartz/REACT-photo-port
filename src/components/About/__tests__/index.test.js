3c9bf9f6d4d16d00e7d3b999639266a57d337fa8
Robert Schwartz <bschwartz23@gmail.com>
2021-07-23 15:08:24 -0700

    add props to Gallery Component

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,23 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
-//Incorporate the React Testing Library
-import { render, cleanup } from '@testing-library/react';
-//Import the jest - dom Package
-import '@testing-library/jest-dom/extend-expect';
-
-
+//ensure that after each test, we won't have any leftover memory data that could give us false results.
 afterEach(cleanup);
-//This will ensure that after each test, we won't have any leftover memory data that could give us false results.
 
 // declare the component we're testing ('About component')
-describe('About component', () => {
-    //first test, does it render?
-    test('renders', () => {
+describe('About component renders', () => {
+    it('renders', () => {
         render(<About />);
     });
-    // second test, create a matching snapshot file
-    test('matches snapshot DOM node structure', () => {
+
+    it('matches snapshot DOM node structure', () => {
         const { asFragment } = render(<About />);
 
         expect(asFragment()).toMatchSnapshot();
