431765ec41250e82c8fc8900ba1e399d5683f45c
Robert Schwartz <bschwartz23@gmail.com>
2021-07-22 18:23:59 -0700

    create Gallery Component file

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Gallery() {
+    return (
+        <section>
+            {/* we want the name and description of the selected category to show up at the top */}
+            <h1>NAME</h1>
+            <p>DESCRIPTION</p>
+        </section>
+    );
+}
+
+
+// export Gallery
+export default Gallery;
