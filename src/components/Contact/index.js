66f4c42fc6b21155181df893572b5228c45a32a4
Robert Schwartz <bschwartz23@gmail.com>
2021-07-23 17:34:08 -0700

    syncing the data from the form to the state of the component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
 
 function ContactForm() {
-//use state to clear the input fields on the component loading.
+    //use state to clear the input fields on the component loading.
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
+
+    //sync the internal state of the component formState with the user input from the DOM
+    function handleChange(e) {
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    }
+
+    console.log(formState);
 
     //create the form DOM elements
     return (
@@ -12,15 +19,15 @@ function ContactForm() {
             <form id="contact-form">
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" defaultValue={name} name="name" />
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" defaultValue={email} name="email" />
+                    <input type="email" defaultValue={email} name="email" onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" defaultValue={message} rows="5" />
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
                 </div>
                 <button type="submit">Submit</button>
             </form>
